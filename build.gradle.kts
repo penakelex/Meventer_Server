bd1737ce5def653a3b84febaea96747bfdb42f82
penakelex <alexeykoninsky129@gmail.com>
2024-02-19 20:26:18 +0700

    Dependencies cleaning + Koin addition

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
+val koin_version: String by project
 val postgres_version: String by project
-val h2_version: String by project
 val exposed_version: String by project
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -32,7 +31,6 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("io.ktor:ktor-server-sessions-jvm")
@@ -40,6 +38,8 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
