c90c3cdec5bf0cba2034806aac1cb04f4578a57b
penakelex <alexeykoninsky129@gmail.com>
2024-02-24 18:41:11 +0700

    Test implementations removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,6 @@ dependencies {
     implementation("io.insert-koin:koin-core:$koin_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("com.sun.mail:jakarta.mail:$jakarta_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    //testImplementation("io.ktor:ktor-server-tests-jvm")
+    //testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
