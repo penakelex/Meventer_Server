56cb4528fd6efda23b774af6cd7a38b587ed1919
penakelex <alexeykoninsky129@gmail.com>
2024-03-21 08:00:43 +0700

    Embedded server update for deployment

--- src/main/kotlin/org/penakelex/Application.kt
+++ src/main/kotlin/org/penakelex/Application.kt
@@ -1,50 +1,21 @@
 package org.penakelex
 
-import com.typesafe.config.ConfigFactory
-import io.ktor.network.tls.certificates.*
 import io.ktor.server.application.*
-import io.ktor.server.config.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.penakelex.plugins.*
-import java.io.File
 
 fun main() {
-    val config = HoconApplicationConfig(ConfigFactory.load())
     val environment = applicationEngineEnvironment {
-        val alias = config.property("ktor.security.ssl.keyAlias").getString()
-        val keyStorePassword = config.property("ktor.security.ssl.keyStorePassword").getString()
-        val privateKeyPassword = config.property("ktor.security.ssl.privateKeyPassword").getString()
-        val sslPort = config.property("ktor.deployment.sslPort").getString().toInt()
-        val keyStore = buildKeyStore {
-            certificate(alias) {
-                password = privateKeyPassword
-                domains = listOf("127.0.0.1", "0.0.0.0", "localhost")
-            }
-        }
-        keyStore.saveToFile(
-            File("keystore.jks"),
-            keyStorePassword
-        )
-        module {
-            module()
-        }
-        sslConnector(
-            keyStore = keyStore,
-            keyAlias = alias,
-            keyStorePassword = { keyStorePassword.toCharArray() },
-            privateKeyPassword = { privateKeyPassword.toCharArray() }
-        ) {
-            port = sslPort
+        connector {
+            host = "0.0.0.0"
+            port = 8080
         }
         connector {
-            host = config.property("ktor.deployment.host").getString()
-            port = sslPort
+            host = "0.0.0.0"
+            port = 8085
         }
-        connector {
-            host = config.property("ktor.deployment.host").getString()
-            port = config.property("ktor.deployment.port").getString().toInt()
-        }
+        module(Application::module)
     }
     embeddedServer(Netty, environment).start(true)
 }
