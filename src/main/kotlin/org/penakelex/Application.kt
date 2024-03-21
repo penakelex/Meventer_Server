e9b56ffa2dbbd4cab752cd718cd7c120429029c6
penakelex <alexeykoninsky129@gmail.com>
2024-03-21 13:29:25 +0700

    Application.kt backup

--- src/main/kotlin/org/penakelex/Application.kt
+++ src/main/kotlin/org/penakelex/Application.kt
@@ -1,23 +1,10 @@
 package org.penakelex
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import org.penakelex.plugins.*
 
-fun main() {
-    embeddedServer(
-        Netty,
-        port = 8080,
-        host = "0.0.0.0"
-    ) {
-        configureDI()
-        configureSockets()
-        configureSerialization()
-        configureSecurity()
-        configureRouting()
-    }.start(true)
-}
+fun main(args: Array<String>) =
+    io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
     configureDI()
